878b6256476ef8a2de4070b1d92cd997b81ed8b2
pinescripter <stevenruhnke2@gmail.com>
2023-10-18 12:33:57 +0200

    add impressive gif function

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,2 +1,3 @@
 import("/public_html/js/menue.js");
 import("/public_html/js/banner.js");
+import("/public_html/js/steven.js");
---
+++ public_html/js/steven.js
@@ -0,0 +1,7 @@
+console.log("test");
+document.getElementById("submit_button").addEventListener("click", function(event){
+    event.preventDefault();
+    let gif=document.getElementById("gif");
+    gif.style.display="block";
+    
+});
