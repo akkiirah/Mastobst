d8d2a09273de4a8b5982bdaa9f026daab1463399
pinescripter <stevenruhnke2@gmail.com>
2023-12-05 22:15:55 +0100

    last fixes from steven

--- public_html/js/steven.js
+++ public_html/js/steven.js
@@ -7,7 +7,7 @@ const tab1 = document.getElementById('tab_1');
 const tab2 = document.getElementById('tab_2');
 const tab3 = document.getElementById('tab_3');
 
-showTab1();
+if (tab1) showTab1();
 
 if (switchToContactButton) {
     switchToContactButton.addEventListener("click", function() {
